070b4a51e2cbba2e7c2bfcf5a06fc75a6134fc33
Karczykowski <mkarczykowski@gmail.com>
2025-10-16 11:31:09 +0200

    add TapOnGreen minigame

---
+++ Assets/Scripts/MiniGames/TapOnGreen.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TapOnGreen : MonoBehaviour
+{
+    [SerializeField] private Image background;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Color startColor = new Color(0.0f, 0.0f, 0.0f);
+    [SerializeField] private Color clickColor = new Color(0.0f, 1.0f, 0.0f);
+
+    [SerializeField] private float baseWaitTime = 5.0f;
+    [SerializeField] private float waitTimeVariation = 0.3f;
+    [SerializeField] private float gameSpeed = 1.0f;
+    [SerializeField] private float reactionTime = 1.0f;
+    [SerializeField] private float colorChangeIntensity = 0.2f;
+
+    private enum State { Waiting, ReactionTime, Success, Fail }
+    private State state = State.Waiting;
+    private float effectiveWaitTime = 5f;
+    private float minEffectiveTime = 1f;
+    private float maxEffectiveTime = 1f;
+    private Coroutine gameplayeCoroutine;
+
+    void Start()
+    {
+        minEffectiveTime = (1f - waitTimeVariation) * baseWaitTime;
+        maxEffectiveTime = (1f + waitTimeVariation) * baseWaitTime;
+        effectiveWaitTime = Random.Range(minEffectiveTime, maxEffectiveTime) / gameSpeed;
+        Debug.Log("Effective wait time for this round: " + effectiveWaitTime);
+
+        gameplayeCoroutine = StartCoroutine(Run());
+    }
+
+    void Update()
+    {
+        if(state == State.ReactionTime)
+        {
+            text.SetText("Klik!");
+            background.color = clickColor;
+        }
+
+        // Color Rotation
+        if(state == State.Waiting)
+        {
+            float pulse = (Mathf.Sin(Time.time * gameSpeed) + 1f) / 2f;
+            float factor = 1f + (pulse - 0.5f) * 2f * colorChangeIntensity;
+            background.color = startColor * factor;
+        }
+
+        bool press = Input.GetMouseButtonDown(0);
+
+        if (!press) return;
+
+        if (state == State.Waiting)
+        {
+            Debug.Log("Przegrana!");
+            text.SetText("Przegrana");
+            state = State.Fail;
+            StopCoroutine(gameplayeCoroutine);
+        }
+        else if (state == State.ReactionTime)
+        {
+            Debug.Log("Wygrana!");
+            text.SetText("Wygrana");
+            state = State.Success;
+            StopCoroutine(gameplayeCoroutine);
+        }
+    }
+
+    private IEnumerator Run()
+    {
+        yield return new WaitForSeconds(effectiveWaitTime);
+        state = State.ReactionTime;
+        yield return new WaitForSeconds(reactionTime);
+        Debug.Log("Przegrana!");
+        text.SetText("Przegrana");
+        state = State.Fail;
+    }
+}
